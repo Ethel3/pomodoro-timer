707c2c3048cd68493308366cf439747c02e3e388
Ethel3 <akrasiethel4@gmail.com>
2022-07-25 23:36:49 +0000

    new file

---
+++ src/Timer.js
@@ -0,0 +1,12 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+function Timer(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Timer;
