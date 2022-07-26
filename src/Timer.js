da3653a7ea3d73633d37b5fe253681966492ced7
Ethel3 <akrasiethel4@gmail.com>
2022-07-26 04:21:28 +0000

    Update

--- src/Timer.js
+++ src/Timer.js
@@ -3,11 +3,14 @@ import "react-circular-progressbar/dist/styles.css";
 import PlayButton from "./PlayButton";
 import PauseButton from "./PauseButton";
 import SettingsButton from "./SettingsButton";
+import {useContext} from "react";
+import SettingsContext from "./SettingsContext";
 
 const red = "#f54e4e";
 const blue = "#0000ff";
 
 function Timer() {
+  const settingsInfo = useContext(SettingsContext);
   return (
     <div>
       <CircularProgressbar
@@ -26,7 +29,7 @@ function Timer() {
         <PauseButton />
       </div>
       <div style={{marginTop: "20px"}}>
-         <SettingsButton />
+         <SettingsButton onClick = {()=> settingsInfo.setShowSettings(true)} />
 
       </div>
     </div>
