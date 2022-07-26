b4b03ff9c2bd2590c4eba7f46f6784c5947aa719
Ethel3 <akrasiethel4@gmail.com>
2022-07-26 13:52:29 +0000

    Updates

--- src/Timer.js
+++ src/Timer.js
@@ -3,7 +3,7 @@ import "react-circular-progressbar/dist/styles.css";
 import PlayButton from "./PlayButton";
 import PauseButton from "./PauseButton";
 import SettingsButton from "./SettingsButton";
-import {useContext} from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import SettingsContext from "./SettingsContext";
 
 const red = "#f54e4e";
@@ -11,26 +11,88 @@ const blue = "#0000ff";
 
 function Timer() {
   const settingsInfo = useContext(SettingsContext);
+
+  const [isPaused, setIsPaused] = useState(true);
+  const [mode, setMode] = useState("Work");
+  const [secondsLeft, setSecondsLeft] = useState(0);
+
+  const secondsLeftRef = useRef(secondsLeft);
+  const isPausedRef = useRef(isPaused);
+  const modeRef = useRef(mode);
+  const nextMode = "Work";
+  const nextSeconds = settingsInfo.WorkMinutes;
+  function tick() {
+    secondsLeftRef.current--;
+    setSecondsLeft(secondsLeftRef.current);
+  }
+
+  useEffect(() => {
+    function switchMode() {
+      nextMode = modeRef.current === "Work" ? "Break" : "Work";
+      nextSeconds =
+        nextMode === "Work"
+          ? settingsInfo.WorkMinutes
+          : settingsInfo.BreakMinutes * 60;
+
+      setMode(nextMode);
+      modeRef.current = nextMode;
+    }
+
+    setSecondsLeft(nextSeconds);
+    secondsLeftRef.current(secondsLeftRef.current);
+
+    const interval = setInterval(() => {
+      if (isPausedRef.current) {
+        return;
+      }
+      if (secondsLeftRef.current === 0) {
+        return switchMode();
+      }
+      tick();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [settingsInfo]);
+
+  const totalSeconds =
+    mode === "Work"
+      ? settingsInfo.WorkMinutes * 60
+      : settingsInfo.BreakMinutes * 60;
+  const percentage = Math.round((secondsLeft / totalSeconds) * 100);
+
+  const minutes = Math.floor(secondsLeft / 60);
+  let seconds = secondsLeft % 60;
+  if (seconds < 10) seconds = "0" + seconds;
   return (
     <div>
       <CircularProgressbar
-        value={70}
-        text={`70%`}
+        value={percentage}
+        text={minutes + ":" + seconds}
         styles={buildStyles({
-          rotation: 0.25,
-          strokeLinecap: "butt",
           textColor: "#fff",
-          pathColor: blue,
-          trailColor: "rgba(225, 225, 225, .2)",
+          pathColor: mode === "Work" ? blue : red,
+          tailColor: "rgba(225, 225, 225, .2)",
         })}
       />
       <div style={{ marginTop: "20px" }}>
-        <PlayButton />
-        <PauseButton />
+        {isPaused ? (
+          <PlayButton
+            onClick={() => {
+              setIsPaused(false);
+              isPausedRef.current = false;
+            }}
+          />
+        ) : (
+          <PauseButton
+            onClick={() => {
+              setIsPaused(true);
+              isPausedRef.current = true;
+            }}
+          />
+        )}
       </div>
-      <div style={{marginTop: "20px"}}>
-         <SettingsButton onClick = {()=> settingsInfo.setShowSettings(true)} />
-
+      <div style={{ marginTop: "20px" }}>
+        <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
       </div>
     </div>
   );
