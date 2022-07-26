5b3bf1a26e59accc95fc70891014ac4697aff969
Ethel3 <akrasiethel4@gmail.com>
2022-07-26 00:31:09 +0000

    updates

--- src/Timer.js
+++ src/Timer.js
@@ -1,12 +1,26 @@
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
-function Timer(){
-    return(
-        <div>
-            
-        </div>
-    )
+const red = "#f54e4e";
+const blue = "#0000ff";
+
+function Timer() {
+  return (
+    <div>
+      <CircularProgressbar
+        value={70}
+        text={`70%`}
+        styles={buildStyles({
+          rotation: 0.25,
+          strokeLinecap: 'butt',
+          textColor: "#fff",
+          pathColor: blue,
+          trailColor: "rgba(225, 225, 225, .2)",
+        })}
+      />
+      <div></div>
+    </div>
+  );
 }
 
 export default Timer;
