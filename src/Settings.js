67ad2e37c390ae55affcc9db7a35673680ccd3d5
Ethel3 <akrasiethel4@gmail.com>
2022-07-26 01:52:58 +0000

    reactslider

--- src/Settings.js
+++ src/Settings.js
@@ -1,7 +1,19 @@
+import ReactSlider from "react-slider";
+
+
 function Settings(){
     return(
         <div style={{textAlign:'left'}}>
             <label>Work Minute:</label>
+            <ReactSlider 
+            className={"slider"}
+            thumbclassName={"thumb"}
+            trackClassName={"track"}
+            value={45}
+            min={1}
+            max={120}
+            
+            />
             <label>Break Minute:</label>
         </div>
     )
