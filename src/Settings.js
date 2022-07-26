463ebc2660c111b65230d6cc349716d5abad4e0c
Ethel3 <akrasiethel4@gmail.com>
2022-07-26 02:21:20 +0000

    sliders

--- src/Settings.js
+++ src/Settings.js
@@ -1,23 +1,28 @@
 import ReactSlider from "react-slider";
-
-
-function Settings(){
-    return(
-        <div style={{textAlign:'left'}}>
-            <label>Work Minute:</label>
-            <ReactSlider 
-            className={"slider"}
-            thumbclassName={"thumb"}
-            trackClassName={"track"}
-            value={45}
-            min={1}
-            max={120}
-            
-            />
-            <label>Break Minute:</label>
-        </div>
-    )
+import ".//Slider.css";
+function Settings() {
+  return (
+    <div style={{ textAlign: "left" }}>
+      <label>Work Minute:</label>
+      <ReactSlider
+        className={"slider"}
+        thumbclassName={"thumb"}
+        trackClassName={"track"}
+        value={45}
+        min={1}
+        max={120}
+      />
+      <label>Break Minute:</label>
+      <ReactSlider
+        className={"slider red"}
+        thumbclassName={"thumb"}
+        trackClassName={"track"}
+        value={45}
+        min={1}
+        max={120}
+      />
+    </div>
+  );
 }
-
 
 export default Settings;
